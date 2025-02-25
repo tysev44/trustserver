f41f0f40e51d8e3bd95c39c422d41eaad976966a
tysev44 <166944856+tysev44@users.noreply.github.com>
2025-02-25 14:00:00 -0800

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const path = require('path');
 const fs = require('fs');
 
 const corsOptions = {
-    origin: ["http://localhost:3000", "http://trustxchanger.com", "https://trustxchanger.com"],
+    origin: ["http://localhost:3000", "http://trustxchanger.com", "https://trustxchanger.com", "*"],
     methods: ['GET', 'POST'],
     credentials: true,
 };
